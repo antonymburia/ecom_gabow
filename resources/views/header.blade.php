eb9eee024432b6a587fcbc827b7f34b303c763d7
antonymburia <antonymburia10@gmail.com>
2022-09-10 18:12:37 +0300

    create navbar logo

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,6 +1,8 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Logo</a>
+        <a class="navbar-brand" href="#">
+            <img src="{{ URL('images/logo.png') }}" alt="">
+        </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -38,9 +40,15 @@
                     <li>
                         <hr class="dropdown-divider">
                     </li>
-                    <li><a class="dropdown-item" href="#">Logout</a></li>
+                    <li><a class="dropdown-item" href="#">logout</a></li>
                 </ul>
             </div>
         </div>
     </div>
 </nav>
+
+<style>
+    img{
+        height: 50px;
+    }
+</style>
