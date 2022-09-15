0b2fd29030666bfdee6506750215b3a8bb8bb293
antonymburia <antonymburia10@gmail.com>
2022-09-15 17:56:41 +0300

    modify cart to prevent adding items without logging in

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,6 +1,12 @@
 <?php
+
 use App\Http\Controllers\ProductController;
-$total = ProductController::cartItem();
+
+$total = 0;
+if (session()->has('user')) {
+    $total = ProductController::cartItem();
+}
+
 ?>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
